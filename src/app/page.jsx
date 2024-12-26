f8489d133d844f810e261969b8c9e65ec1ad5be4
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2024-12-26 08:26:28 +0100

    Setup colors, fonts, and some dependencies

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h3 className="text-primaryGreen text-center p-5">
+        Welcome to Terra Balance
+      </h3>
+    </div>
+  );
+};
+
+export default Home;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,30 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primaryGreen: "var(--primary-green)",
+        primaryBrown: "var(--primary-brown)",
+        secondaryGreen: "var(--secondary-green)",
+        secondaryBrown: "var(--secondary-brown)",
+        accentYellow: "var(--accent-yellow)",
+        accentBlue: "var(--accent-blue)",
+        neutralWhite: "var(--neutral-white)",
+        neutralGray: "var(--neutral-gray)",
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
+
+export default config;
